9e0257e37eb005c041935cd697ebdaba722e9ac0
Luiz Fernando L R Ferreira <lewisferreira@users.noreply.github.com>
2015-06-12 09:50:51 -0300

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,12 @@
+from myServer import db
+
+class estufa(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	local = db.Column(db.String(100))
+	meds = db.relationship('Measures')
+
+class Measure(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	est_id = db.Column(db.Integer, db.ForeignKey('estufa.id'))
+	temp = db.Column(db.Float)
+	umi = db.Column(db.Float)
